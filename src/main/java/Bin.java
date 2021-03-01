ac888cee43e3172fd8e6882d9ebf72d455710070
Tanguy <tang.caillaud@gmail.com>
2021-03-01 12:09:22 +0100

    Objets

---
+++ src/main/java/Bin.java
@@ -0,0 +1,15 @@
+public class Bin {
+    private int length;
+
+    public Bin(int length){
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
--- src/main/java/BinPacking.java
+++ src/main/java/BinPacking.java
@@ -1,2 +1,17 @@
-public class BinPacking {
+public class BinPacking{
+
+    static int infBorne (int[] items, Bin bin){
+        int length = bin.getLength();
+        int sum = 0;
+        for(int i =0; i<items.length; i++){
+            sum+=items[i];
+        }
+        float borne = sum/length;
+        return borne;
+    }
+
+    public static void main(String args[]){
+
+    }
+
 }
