e1865a97c0d2d25cf3c7496f062a8d5c44b2276b
Tanguy <tang.caillaud@gmail.com>
2021-03-01 12:24:38 +0100

    Jdk + borne

--- src/main/java/BinPacking.java
+++ src/main/java/BinPacking.java
@@ -1,17 +1,25 @@
-public class BinPacking{
+public class BinPacking {
 
-    static int infBorne (int[] items, Bin bin){
-        int length = bin.getLength();
-        int sum = 0;
-        for(int i =0; i<items.length; i++){
-            sum+=items[i];
+    static int infBorne(int[] items, Bin bin) {
+        double length = bin.getLength();
+        double sum = 0;
+        for (int i = 0; i < items.length; i++) {
+            sum += items[i];
         }
-        float borne = sum/length;
+        int borne = (int) Math.ceil(sum / length);
         return borne;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        Bin bin = new Bin(5);
+        int[] items1 = new int[]{5, 5, 5, 5, 6};
+        int[] items2 = new int[]{5, 5, 5, 5, 5};
+        int[] items3 = new int[]{5, 5, 5, 5, 4};
 
+        int result1 = infBorne(items1, bin);
+        int result2 = infBorne(items2, bin);
+        int result3 = infBorne(items3, bin);
+
+        System.out.println(result1 + " " + result2 + " " + result3);
     }
-
 }
