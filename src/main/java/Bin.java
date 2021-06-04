ad00a1ed98d4767c6c796e1bbf720b77eb2ea9cc
Tanguy <tang.caillaud@gmail.com>
2021-06-04 10:54:44 +0200

    Changes by Félix

--- src/main/java/Bin.java
+++ src/main/java/Bin.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Bin {
     private final int length;
     private int spaceLeft;
-    private Set<Item> items;
+    private List<Item> items;
 
     public Bin(int length){
         this.length = length;
         this.spaceLeft = length;
-        items = new HashSet<>();
+        items = new ArrayList<>();
     }
 
     public boolean canAdd(Item item){
@@ -48,5 +50,19 @@ public class Bin {
         String concat = "";
         for(Item item: items){ concat += item.toString() + '-'; }
         return concat.substring(0, concat.length() - 1);
+    }
+
+    public int getFitness() {
+        int f = 0;
+        for(Item i : items) f += i.getSize();
+        return f;
+    }
+
+    public Item getItem(int index){
+        return items.get(index);
+    }
+
+    public List<Item> getItems(){
+        return items;
     }
 }
